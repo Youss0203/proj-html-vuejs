96dc7afee6d6794d96ac79d70e90ce658048fd5c
Youss0203 <youssefhalibi003@gmail.com>
2024-01-08 12:47:12 +0100

    completata divisione in macrostrutture

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,14 @@
 <template lang="">
-    <div>
-        <h1>Footer</h1>
-    </div>
+    <section class="upper">
+        <div class="column"></div>
+        <div class="column"></div>
+        <div class="column"></div>
+        <div class="column"></div>
+        <div class="column"></div>
+    </section>
+    <section class="contacts">
+
+    </section>
 </template>
 <script>
 export default {
@@ -9,5 +16,17 @@ export default {
 }
 </script>
 <style lang="scss">
-    
+    section.upper{
+        height: 400px;
+        display:flex;
+
+        .column{
+            width:20%;
+            border:solid black 1px;
+        }
+    }
+    section.contacts{
+        height: 50px;
+        border-top: 1px solid #7e8890;
+    }
 </style>
